7ef830d09363ab4e3a2e03b42d4d0d4e90aff4c4
Ahmed Ali <70485778+AhmedAli777@users.noreply.github.com>
2023-12-28 16:09:10 +0300

    Update EEPROM_int.h

--- E_TWI_EEPROM/HAL/EEPROM/EEPROM_int.h
+++ E_TWI_EEPROM/HAL/EEPROM/EEPROM_int.h
@@ -10,10 +10,14 @@
 #include "../../MCAL/TWI/TWI_int.h"
 #include "EEPROM_config.h"
 
-void EEPROM_voidRead(u8 Address,u8* ptr);
-void EEPROM_voidWrite(u8 Address,u8 u8data);
+void EEPROM_voidRead(u8 u8BlockNo,u8 Address,u8* ptr);
+void EEPROM_voidWrite(u8 u8BlockNo,u8 Address,u8 u8data);
 
-void EEPROM_voidReadString(u8 Address,u8* ptr);
-void EEPROM_voidWriteString(u8 Address,u8 *ptr);
+void EEPROM_voidReadString(u8 u8BlockNo,u8 Address,u8* ptr);
+void EEPROM_voidWriteString(u8 u8BlockNo,u8 Address,u8 *ptr);
+
+void EEPROM_voidERASEBLOCK(u8 U8BlockNo);
+void EEPROM_voidERASEALLDATA(u8 u8Pass);
+
 
 #endif /* HAL_EEPROM_EEPROM_INT_H_ */
