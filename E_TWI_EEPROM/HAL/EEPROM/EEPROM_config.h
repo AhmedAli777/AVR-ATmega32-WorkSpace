ae4233f6b7241fae16a25e30a462c999ce9551de
Ahmed Ali <70485778+AhmedAli777@users.noreply.github.com>
2023-12-29 15:00:33 +0300

    Update EEPROM_config.h

--- E_TWI_EEPROM/HAL/EEPROM/EEPROM_config.h
+++ E_TWI_EEPROM/HAL/EEPROM/EEPROM_config.h
@@ -9,8 +9,6 @@
 #define HAL_EEPROM_EEPROM_CONFIG_H_
 
 #define EEPROM_Adress        0x50 //01010000
-#define MaxBlockNo           7
-#define MaxWordAddress       255
-#define ERASEPASS            0X22
+
 
 #endif /* HAL_EEPROM_EEPROM_CONFIG_H_ */
